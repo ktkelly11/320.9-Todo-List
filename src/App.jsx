621502615396c9c4356117a2e4b11ce919e99133
Katherine Kelly <ktkelly11@gmail.com>
2024-06-16 10:46:49 -0400

    Started creating App

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,14 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Create Todo List</h1>
+      <form className="new-item-form">
+        <input type="text" id="item" placeholder="Add Task" />
+      </form>
+      <button className="button">Add</button>
+      <hr />
     </>
   );
 }

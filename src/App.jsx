a82352a462bd799a5667137824c3f75808bdfe7f
Katherine Kelly <ktkelly11@gmail.com>
2024-06-16 15:43:16 -0400

    Added comments

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,4 @@ function App() {
   );
 }
 
-const initialTodos = [
-  { number: 1, item: "Review React" },
-  { number: 2, item: "Plan Capstone" },
-  { number: 3, item: "Wireframe for Capstone" },
-  { number: 4, item: "React SBA" },
-  { number: 5, item: "Complete Capstone" },
-  { number: 6, item: "Present Capstone" },
-  { number: 7, item: "Graduate" },
-];
-
 export default App;
--- src/todoList.jsx
+++ src/todoList.jsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
+
 export default function ToDoList() {
+  const [todos, setTodos] = useState(initialTodos);
+  //  2. A list of "todo" items
+  //   const initialTodos = [
+  //     { number: 1, text: "Review React", completed: false },
+  //     { number: 2, text: "Plan Capstone", completed: false },
+  //     { number: 3, text: "Wireframe for Capstone", completed: false },
+  //     { number: 4, text: "React SBA", completed: false },
+  //     { number: 5, text: "Complete Capstone", completed: false },
+  //     { number: 6, text: "Present Capstone", completed: false },
+  //     { number: 7, text: "Graduate", completed: false },
+  //   ];
+
   return (
     <>
       {/* 1. A heading labeling it a "todo list" */}
       <h1>Create Todo List</h1>
       <form className="new-item-form">
+        {/* 3. An input element that creates new todo items and adds them to the list. */}
         <input type="text" id="item" placeholder="Add Task" />
       </form>
       <button className="button">Add</button>
@@ -11,10 +26,13 @@ export default function ToDoList() {
       <ul className="list">
         <li>
           <label>
+            {/* 2.1 A checkbox next to it which indicates whether it is "complete." */}
             <input type="checkbox" />
             Item 1
           </label>
+          {/* 3. An "edit" button that replaces the todo string with a text input used to edit the todo. */}
           <button className="button edit">Edit</button>
+          {/* 2.2 A "delete" button next to it which removes it from the list. */}
           <button className="button delete">Delete</button>
         </li>
       </ul>

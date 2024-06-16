7f666628e00c325868d0d95ad5e58cbfa817648d
Katherine Kelly <ktkelly11@gmail.com>
2024-06-16 11:08:00 -0400

    Set up App

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [newItem, setNewItem] = useState("");
+
   return (
     <>
       <h1>Create Todo List</h1>
@@ -10,6 +12,24 @@ function App() {
       </form>
       <button className="button">Add</button>
       <hr />
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="button edit">Edit</button>
+          <button className="button delete">Delete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 2
+          </label>
+          <button className="button edit">Edit</button>
+          <button className="button delete">Delete</button>
+        </li>
+      </ul>
     </>
   );
 }
